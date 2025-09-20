6a6994ad141fede0d2d343e6faeac82ad844f3e6
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-09-20 01:14:25 -0300

    feat: improve main file

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-export class Tinid {
-  private static readonly chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+export const DEFAULT_CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
+export class Tinid {
   /**
    * Generates a random alphanumeric string ID.
    * @param length Length of the ID. Default is 8.
+   * @param charset Character set to use. Default is alphanumeric.
    * @returns Random ID string.
    */
-  static generate(length: number = 8, charset: string = Tinid.chars): string {
+  static generate(length: number = 8, charset: string = DEFAULT_CHARSET): string {
     let result = '';
     const charsLength = charset.length;
 
@@ -19,4 +20,9 @@ export class Tinid {
   }
 }
 
-export const tinid = (length?: number) => Tinid.generate(length);
+/**
+ * Lightweight ID generator function.
+ * @param length Length of the ID. Default is 8.
+ * @returns Random ID string.
+ */
+export const tinid = (length?: number): string => Tinid.generate(length);
